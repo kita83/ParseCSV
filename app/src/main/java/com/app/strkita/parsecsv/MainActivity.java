3a1d2772013ea559b7955c263887aa79645652d0
strkita <kita83@gmail.com>
2017-07-09 10:11:29 +0900

    first commit

---
+++ app/src/main/java/com/app/strkita/parsecsv/MainActivity.java
@@ -0,0 +1,42 @@
+package com.app.strkita.parsecsv;
+
+import android.content.res.AssetManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView tv01 = (TextView)findViewById(R.id.tv01);
+        AssetManager assetManager = getResources().getAssets();
+        try {
+            InputStream is = assetManager.open("e3tsurumi1701.csv");
+            InputStreamReader inputStreamReader = new InputStreamReader(is);
+            BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+            String line = "";
+            StringBuilder strBuild = new StringBuilder();
+            while ((line = bufferedReader.readLine()) != null) {
+                StringTokenizer st = new StringTokenizer(line, ",");
+                while (st.hasMoreTokens()) {
+                    strBuild.append(st.nextToken());
+                    strBuild.append(",");
+                }
+                strBuild.append("\n");
+            }
+            tv01.setText(strBuild.toString());
+            bufferedReader.close();
+         } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
